ede6273077869e9e2b3de9ef634bd42fb659def1
FLampacrescia <flampacrescia@gmail.com>
2025-05-14 05:28:34 -0300

    Add Auth middleware to order routes

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const orderController = require ('../controllers/order.controller');
 const { isAuth } = require('../middlewares/isAuth');
 
-router.get('/orders', orderController.getOrders)
+router.get('/orders', [ isAuth ], orderController.getOrders)
 router.post('/orders', [ isAuth ], orderController.createOrder)
 
 module.exports = router;
