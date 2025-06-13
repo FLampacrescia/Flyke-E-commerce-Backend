c680022a5b522e54ffd3bdcf4ca04240a1507afd
FLampacrescia <flampacrescia@gmail.com>
2025-06-13 18:07:40 -0300

    Fix delete route in store.routes

--- routes/store.routes.js
+++ routes/store.routes.js
@@ -3,7 +3,7 @@ const storeController = require('../controllers/store.controller');
 
 router.get('/stores', storeController.getStores);
 router.post('/stores', storeController.addStore);
-router.delete('/stores', storeController.deleteStore);
+router.delete('/stores:id', storeController.deleteStore);
 router.put('/stores/:id', storeController.updateStoreById);
 
 module.exports = router;
