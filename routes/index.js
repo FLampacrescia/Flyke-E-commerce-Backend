09c894f4d41f92c9fa2ea06cd1b01ad8a6427442
FLampacrescia <flampacrescia@gmail.com>
2025-06-13 17:54:58 -0300

    Add storeRouter

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const productRouter = require("./product.routes");
 const userRouter = require("./user.routes");
 const categoryRouter = require("./category.routes");
 const orderRouter = require ('./order.routes');
+const storeRouter = require ('./store.routes');
 
 
-module.exports = [ productRouter, userRouter, categoryRouter, orderRouter ]
+module.exports = [ productRouter, userRouter, categoryRouter, orderRouter, storeRouter ]
