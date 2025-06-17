515e6592454ad4e42ae0089b2786e0b41f07b3f7
FLampacrescia <flampacrescia@gmail.com>
2025-06-17 02:45:24 -0300

    Add middlewares to stores.routes

--- routes/store.routes.js
+++ routes/store.routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const storeController = require('../controllers/store.controller');
 
 router.get('/stores', storeController.getStores);
-router.post('/stores', storeController.addStore);
-router.delete('/stores/:id', storeController.deleteStoreById);
-router.put('/stores/:id', storeController.updateStoreById);
+router.post('/stores', [ isAuth, isAdmin ],storeController.addStore);
+router.delete('/stores/:id', [ isAuth, isAdmin ],storeController.deleteStoreById);
+router.put('/stores/:id', [ isAuth, isAdmin ], storeController.updateStoreById);
 
 module.exports = router;
