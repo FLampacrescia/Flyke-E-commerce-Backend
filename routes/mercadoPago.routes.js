7ac58ce75389d121e2e0b73e2ae7379583d2b2e4
FLampacrescia <flampacrescia@gmail.com>
2025-06-29 06:59:58 -0300

    Fix mercadoPago.routes.js

--- routes/mercadoPago.routes.js
+++ routes/mercadoPago.routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const mercadoPagoController = require ('../controllers/mercadoPago.controller');
 
 // Ruta para crear una preferencia y devolver el preferenceId
-router.post('/preference', CreatePreferenceId);
+router.post('/preference', mercadoPagoController.CreatePreferenceId);
 
 // Ruta para procesar un pago con los datos del Payment Brick
-router.post('/process-payment', createPayment);
+router.post('/process-payment', mercadoPagoController.createPayment);
 
 module.exports = router;
